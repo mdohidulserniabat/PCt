c906c75356dcd479074258aaa790cf462f7fedd3
MD Ohidul Islam <42583808+mdohidulserniabat@users.noreply.github.com>
2022-07-16 19:08:17 +0600

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,8 @@
+/* ==================================== typing animation =============================== */
+/* ==================================== typing animation =============================== */
+var typed =new Typed(".typing", {
+  strings:["Web Designer","Web Developer","MERN Stack Developer"],
+  typeSpeed: 150,
+  BackSpeed: 60,
+  loop:true
+})
